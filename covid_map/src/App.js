0dcaa737dfac3cadded195388852bc6094f87127
Arturo Tepale <atepaleag@gmail.com>
2020-04-22 01:13:32 -0500

    BRUh

--- covid_map/src/App.js
+++ covid_map/src/App.js
@@ -1,17 +1,111 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   GoogleMap,
   withScriptjs,
   withGoogleMap,
   InfoWindow,
+  Marker,
 } from "react-google-maps";
-import Markers from "./components/Markers.js";
+
 
 function Map() {
-  const [selectedCity, setSelectedCity] = useState(null);
+  const [selectedCity, setSelectedCity] = useState({
+    lat: null,
+    lng: null,
+    cty: null,
+    cnt: null,
+    cnf: null,
+    rcv: null,
+    dth: null,
+  });
+
+  function pass(lt, ln, ct, cn, cf, rv, dt) {
+    if (!isNaN(lt) || lt != null) {
+      if (!isNaN(ln) || ln != null) {
+        setSelectedCity({
+          lat: lt,
+          lng: ln,
+          cty: ct,
+          cnt: cn,
+          cnf: cf,
+          rcv: rv,
+          dth: dt,
+        });
+      }
+    }
+  }
+  const [ccases, setCCases] = useState(null);
+  //var obtain = false;
+  //const [data, setData] = useState({ cases: null });
+  useEffect(() => {
+    fetch(
+      "https://opendata.arcgis.com/datasets/bbb2e4f589ba40d692fab712ae37b9ac_1.geojson"
+    )
+      .then((response) => response.json())
+      .then(
+        (responseJson) => setCCases(responseJson)
+        // jsonData is parsed json object received from url
+      );
+  }, [setCCases]);
   return (
-    <GoogleMap defaultZoom={3} defaultCenter={{ lat: 0, lng: 0 }}>
-      <Markers />;
+    <GoogleMap defaultZoom={4} defaultCenter={{ lat: 37.0902, lng: -95.7129 }}>
+      {ccases &&
+        ccases.features.map((place) => (
+          <Marker
+            key={place.properties.OBJECTID}
+            position={{
+              lat: place.properties.Lat,
+              lng: place.properties.Long_,
+            }}
+            onClick={() =>
+              pass(
+                place.properties.Lat,
+                place.properties.Long_,
+                place.properties.Province_State,
+                place.properties.Country_Region,
+                place.properties.Confirmed,
+                place.properties.Recovered,
+                place.properties.Deaths
+              )
+            }
+          />
+        ))}
+      {selectedCity.lat != null && selectedCity.lng != null && (
+        <InfoWindow
+          position={{
+            lat: selectedCity.lat,
+            lng: selectedCity.lng,
+          }}
+          onCloseClick={() => {
+            setSelectedCity({
+              lat: null,
+              lng: null,
+              cty: null,
+              cnt: null,
+              cnf: null,
+              rcv: null,
+              dth: null,
+            });
+          }}
+        >
+          <div>
+            <h2>{selectedCity.cty}</h2>
+            <h3>{selectedCity.cnt}</h3>
+            <p>
+              Confirmed: {selectedCity.cnf}
+              <br />
+            </p>
+            <p>
+              Recovered: {selectedCity.rcv}
+              <br />
+            </p>
+            <p>
+              Deaths: {selectedCity.dth}
+              <br />
+            </p>
+          </div>
+        </InfoWindow>
+      )}
     </GoogleMap>
   );
 }
